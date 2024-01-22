b929bfd47e76be1eaa308ba971ba27e73378981e
Digital-Haunt <chrisjhi@umich.edu>
2024-01-21 21:08:44 -0500

    Ball and bar fully working as intended
    Unless I missed any bugs

--- game/Scenes/Breakout/Ball.gd
+++ game/Scenes/Breakout/Ball.gd
@@ -3,22 +3,36 @@ extends KinematicBody2D
 class_name Ball
 
 const VELOCITY_LIMIT = 40
-
+var speed_up_factor = 1.05
 export var ball_speed = 20
 export var lives = 3
+var velocity
+var is_ball_started = false
 
-var speed_up_factor = 1.05
 
 onready var collision_shape_2d = $CollisionShape2D
-
 var start_position: Vector2
 
 func _ready():
 	start_position = position
+	
+	if(is_ball_started):
+		velocity = Vector2(rand_range(-1, 1), rand_range(-.1, -1)).normalized() * ball_speed
+	
+
+func _physics_process(delta):
+	
+	var collision
+	if(is_ball_started):
+		collision = move_and_collide(velocity * ball_speed * delta)
+	
+	if(!collision):
+		return
+	
+	velocity = velocity.bounce(collision.normal)
 
 func start_ball():
+	is_ball_started = true
 	position = start_position
 	randomize()
-	
-	var velocity = Vector2(rand_range(-1, 1), rand_range(-.1, -1)).normalized()
-	
+	velocity = Vector2(rand_range(-1, 1), rand_range(-.1, -1)).normalized() * ball_speed
--- game/Scenes/Breakout/paddle.gd
+++ game/Scenes/Breakout/paddle.gd
@@ -2,11 +2,16 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 var speed
+var is_ball_started = false
 
+onready var ball = $"../Ball" as Ball
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	speed = 250
+	speed = 350
+
+func _physics_process(delta):
+	velocity.y = 0
 
 func get_input():
 	velocity.x = 0
@@ -17,9 +22,12 @@ func get_input():
 		velocity.x = speed
 	if left:
 		velocity.x = -speed
+	
+	if velocity != Vector2.ZERO && !is_ball_started:
+		ball.start_ball()
+		is_ball_started = true
 
 func _process(delta):
 	
 	get_input()
-	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
