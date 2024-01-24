610b111de980f6040d77470d7cf2a0cc4d94cc03
Digital-Haunt <chrisjhi@umich.edu>
2024-01-23 20:09:49 -0500

    Additional breakout changes

--- game/Scenes/Breakout/Ball.gd
+++ game/Scenes/Breakout/Ball.gd
@@ -6,6 +6,8 @@ const VELOCITY_LIMIT = 40
 var speed_up_factor = 1.05
 export var ball_speed = 20
 export var lives = 3
+var deathZone: DeathZone
+
 var velocity
 var is_ball_started = false
 
@@ -14,10 +16,11 @@ onready var collision_shape_2d = $CollisionShape2D
 var start_position: Vector2
 
 func _ready():
-	start_position = position
-	
+	start_position = position	
 	if(is_ball_started):
 		velocity = Vector2(rand_range(-1, 1), rand_range(-.1, -1)).normalized() * ball_speed
+	#deathZone.life_lost.connect("on_life_lost")
+	deathZone.life_lost.connect("life_lost", self, on_life_lost())
 	
 
 func _physics_process(delta):
@@ -36,3 +39,16 @@ func start_ball():
 	position = start_position
 	randomize()
 	velocity = Vector2(rand_range(-1, 1), rand_range(-.1, -1)).normalized() * ball_speed
+
+func on_life_lost():
+	lives -= 1
+	
+	if lives == 0:
+		pass
+		#GAME OVER
+	else:
+		reset_ball()
+
+func reset_ball():
+	position = start_position
+	velocity = Vector2.ZERO
