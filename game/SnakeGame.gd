4a2fe10145820d868b0b7eb3513a7e110df5b86c
cjlovelace <cjlovela@umich.edu>
2024-01-23 20:35:27 -0500

    Updated movement, added keyboard controls.

--- game/SnakeGame.gd
+++ game/SnakeGame.gd
@@ -9,13 +9,17 @@ var game_started : bool = false
 var cells : int = 20
 var cell_size : int = 50
 
+#food
+var food_pos: Vector2
+var regen_food : bool = true
+
 #snake
 var old_data : Array
 var snake_data : Array
 var snake : Array
 
 #movement
-var start_pos = Vector2(2, 2)
+var start_pos = Vector2(8, 8)
 var up = Vector2(0, -1)
 var down = Vector2(0, 1)
 var left = Vector2(-1, 0)
@@ -33,6 +37,7 @@ func new_game():
 	move_direction = up
 	can_move = true
 	generate_snake()
+	move_food()
 	
 func generate_snake():
 	old_data.clear()
@@ -49,6 +54,83 @@ func add_segment(pos):
 	add_child(SnakeSegment)
 	snake.append(SnakeSegment)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	move_snake()
+
+func move_snake():
+	if can_move:
+		if Input.is_action_just_pressed("move_down") and move_direction != up:
+			move_direction = down
+			can_move = false
+			if not game_started:
+				start_game()
+		elif Input.is_action_just_pressed("move_up") and move_direction != down:
+			move_direction = up
+			can_move = false
+			if not game_started:
+				start_game()
+		elif Input.is_action_just_pressed("move_left") and move_direction != right:
+			move_direction = left
+			can_move = false
+			if not game_started:
+				start_game()
+		elif Input.is_action_just_pressed("move_right") and move_direction != left:
+			move_direction = right
+			can_move = false
+			if not game_started:
+				start_game()
+				
+func start_game():
+	game_started = true
+	$MoveTimer.start()
+
+
+func _on_MoveTimer_timeout():
+	can_move = true
+	old_data = [] + snake_data
+	snake_data[0] += move_direction
+	for i in range(len(snake_data)):
+		if i > 0:
+			snake_data[i] = old_data[i - 1]
+		snake[i].position = (snake_data[i] * cell_size) + Vector2(0, cell_size)
+	check_out_of_bounds()
+	check_self_eaten()
+	check_food_eaten()
+
+func check_out_of_bounds():
+	if snake_data[0].x < 0 or snake_data[0].x > cells - 1 or snake_data[0].y < 0 or snake_data[0].y > cells - 1:
+		end_game()
+		
+func check_self_eaten():
+	for i in range(1, len(snake_data)):
+		if snake_data[0] == snake_data[i]:
+			end_game()
+			
+func check_food_eaten():
+	#if snake eats the food, add a segment and move the food
+	if snake_data[0] == food_pos:
+		score += 1
+		$Hud.get_node("ScoreLabel").text = "SCORE: " + str(score)
+		add_segment(old_data[-1])
+		move_food()
+		
+func move_food():
+	while regen_food:
+		regen_food = false
+		food_pos = Vector2(rand_range(0, cells - 1), rand_range(0, cells - 1))
+		for i in snake_data:
+			if food_pos == i:
+				regen_food = true
+	$Food.position = (food_pos * cell_size)+ Vector2(0, cell_size)
+	regen_food = true
+
+func end_game():
+	$GameOverMenu.show()
+	$MoveTimer.stop()
+	game_started = false
+	get_tree().paused = true
+
+
+func _on_game_over_menu_restart():
+	new_game()
