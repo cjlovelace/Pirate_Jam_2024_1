395bc1789f8575b14e9d259b9fec5815a3aadc47
cjlovelace <cjlovela@umich.edu>
2024-01-26 19:43:53 -0500

    Finished basic snake game functionality in popup.

--- game/SnakePopupScene.gd
+++ game/SnakePopupScene.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 var score : int
-var winning_score : int = 3
+var winning_score : int = 30
 var game_started : bool = false
 var already_won : bool = false
+var lost : bool = false
 
 #grid
 var cells : int = 40
@@ -34,11 +35,17 @@ func _ready():
 	new_game()
 
 func new_game():
+	old_data.clear()
+	snake_data.clear()
+	snake.clear()
+	$SnakePopupTest/Menu.hide()
 	score = 0
 	generate_snake()
 	move_food()
 	move_direction = up
 	can_move = true
+	lost = false
+	$SnakePopupTest/ScoreLabel.text = "Score: " + str(score)
 
 func generate_snake():
 	old_data.clear()
@@ -62,6 +69,7 @@ func move_food():
 		for i in snake_data:
 			if food_pos == i:
 				regen_food = true
+	$SnakePopupTest/Sprite/SnakeBox/Food.texture = load("res://apple.png")
 	$SnakePopupTest/Sprite/SnakeBox/Food.position = food_pos
 	regen_food = true
 	print("Food: " + str(food_pos) + " Snake: " + str(snake[0].position))
@@ -71,7 +79,7 @@ func _process(_delta):
 	move_snake()
 
 func move_snake():
-	if can_move and score < winning_score:
+	if can_move and score < winning_score and !lost:
 		if Input.is_action_just_pressed("move_down") and move_direction != up:
 			move_direction = down
 			can_move = false
@@ -97,6 +105,11 @@ func start_game():
 	game_started = true
 	$MoveTimer.start()
 
+func check_self_eaten():
+	for i in range(1, len(snake_data)):
+		if snake_data[0] == snake_data[i]:
+			end_game()
+			
 func _on_MoveTimer_timeout():
 	can_move = true
 	old_data = [] + snake_data
@@ -107,8 +120,48 @@ func _on_MoveTimer_timeout():
 		if i > 0:
 			snake_data[i] = old_data[i - 1]
 		snake[i].position = snake_data[i]
-	if ((snake[0].position.x >= food_pos.x - 5) and (snake[0].position.x <= food_pos.x + 25) and
-		(snake[0].position.y >= food_pos.y - 5) and (snake[0].position.y <= food_pos.y + 25)):
-			ate_counter+= 1
-			#print("She ate " + str(ate_counter))
-	#print("Snake: " + str(snake[0].position))
+	check_out_of_bounds()
+	if (((snake[0].position.x >= food_pos.x - 15) and (snake[0].position.y <= food_pos.y + 15)) and
+		((snake[0].position.x <= food_pos.x + 15) and (snake[0].position.y >= food_pos.y - 15))):
+			food_eaten()
+			
+func food_eaten():
+	score += 1
+	$SnakePopupTest/ScoreLabel.text = "Score: " + str(score)
+	add_segment(old_data[-1])
+	if score < winning_score:
+		move_food()
+	else:
+		end_game()
+		
+func add_segment(pos):
+	var new_sprite = Sprite.new()
+	new_sprite.texture = load("res://SnakeSeg.png")
+	new_sprite.position = pos
+	snake_data.append(new_sprite.position)
+	snake.append(new_sprite)
+	$SnakePopupTest.add_child(new_sprite)
+
+func check_out_of_bounds():
+	if snake_data[0].x <= 10 or snake_data[0].x >= 480 or snake_data[0].y <= 45 or snake_data[0].y >= 335:
+		$MoveTimer.stop()
+		end_game()
+		
+func end_game():
+	$MoveTimer.stop()
+	game_started = false
+	lost = true
+	
+	for i in range(len(snake_data)):
+		snake[i].texture = null
+		
+	if score == winning_score:
+		already_won = true
+		$SnakePopupTest/Menu/WinStatus.text = "YOU WIN!"
+	else:
+		$SnakePopupTest/Menu/WinStatus.text = "GAME OVER!"
+	$SnakePopupTest/Menu.show()
+
+func _on_PlayBtn_button_up():
+	$SnakePopupTest/Menu.hide()
+	new_game()
